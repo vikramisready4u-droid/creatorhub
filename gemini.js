b0ace44ff5ea207cda76844b1509874226716e0d
vikramisready4u-droid <vikramisready4u@gmail.com>
2026-05-30 10:22:12 +0530

    Add files via upload

---
+++ gemini.js
@@ -0,0 +1,54 @@
+export default async function handler(req, res) {
+
+  const apiKey = process.env.GEMINI_API_KEY;
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  const { prompt } = req.body;
+
+  try {
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    const text =
+      data.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "No response";
+
+    res.status(200).json({
+      result: text
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
+}
